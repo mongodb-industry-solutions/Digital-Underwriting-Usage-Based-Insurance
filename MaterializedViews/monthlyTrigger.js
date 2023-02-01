b3e2bfbe1bb76a63b51f3b720a99d725deab6e0e
LucaNapoli <luc.napoli@gmail.com>
2023-02-01 11:04:11 +0100

    Update monthlyTrigger.js

--- MaterializedViews/monthlyTrigger.js
+++ MaterializedViews/monthlyTrigger.js
@@ -13,23 +13,12 @@ const agg = [
       }
     }
   }, {
-    '$lookup': {
-      'from': 'customerPolicy', 
-      'localField': '_id.customerId', 
-      'foreignField': 'customerId', 
-      'as': 'baseMonthlyPremium'
-    }
-  }, {
     '$project': {
-      'year': '$_id.year', 
-      'month': '$_id.month', 
-      'customerId': '$_id.customerId', 
       'totalDistance': {
         '$round': [
           '$totalDistance', 1
         ]
-      }, 
-      'baseMonthlyPremium': '$baseMonthlyPremium.baseMonthlyPremium'
+      }
     }
   }, {
     '$addFields': {
