d461cfb56a0139f7906ffd407dcc6166bf760336
LucaNapoli <luc.napoli@gmail.com>
2023-03-07 11:48:40 +0100

    Update monthlyTrigger.js

--- src/MaterializedViews/monthlyTrigger.js
+++ src/MaterializedViews/monthlyTrigger.js
@@ -1,4 +1,4 @@
-exports = function() {
+exports = async function() {
 
 const agg = [
   {
@@ -34,5 +34,6 @@ const agg = [
 
 const coll = context.services.get("mongodb-atlas").db('digital_underwriting').collection('customerTripDaily');
 const cursor = coll.aggregate(agg);
+return cursor;
   
 };
