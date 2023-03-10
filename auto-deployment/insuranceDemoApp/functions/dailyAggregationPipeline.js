0ff2be4dec0f57e14b66ed9f19ef7006df375350
Jeff Needham <38022295+dataJeff@users.noreply.github.com>
2023-03-10 12:52:26 -0500

    Update dailyAggregationPipeline.js

--- auto-deployment/insuranceDemoApp/functions/dailyAggregationPipeline.js
+++ auto-deployment/insuranceDemoApp/functions/dailyAggregationPipeline.js
@@ -1,55 +1,43 @@
 exports = async function() {
 
     const agg = [
-      {
-        '$project': {
-          'date': {
-            '$dateToParts': {
-              'date': '$timestamp'
-            }
-          },
-          'customerId': 1,
-          'milesDriven': 1,
-          'speedUnit': 1
+  {
+    '$project': {
+      'date': {
+        '$dateToParts': {
+          'date': '$timestamp'
         }
-      }, {
-        '$group': {
-          '_id': {
-            'date': {
-              'year': '$date.year',
-              'month': '$date.month',
-              'day': '$date.day'
-            },
-            'customerId': '$customerId'
-          },
-          'averageDistance': {
-            '$avg': '$milesDriven'
-          },
-          'totalDistance': {
-            '$sum': '$milesDriven'
-          }
-        }
-      }, {
-        '$project': {
-          'customerId': '$_id.customerId',
-          'year': '$_id.date.year',
-          'month': '$_id.date.month',
-          'day': '$_id.date.day',
-          'averageDistance': 1,
-          'totalDistance': 1,
-          '_id': 1
-        }
-      }, {
-        '$addFields': {
-          'viewUpdateTimestamp': new Date()
-        }
-      }, {
-        '$merge': {
-          'into': 'customerTripDaily',
-          'whenMatched': 'replace'
-        }
+      }, 
+      'metadata': 1, 
+      'milesDriven': 1, 
+    }
+  }, {
+    '$group': {
+      '_id': {
+        'date': {
+          'year': '$date.year', 
+          'month': '$date.month', 
+          'day': '$date.day'
+        }, 
+        'customerId': '$metadata.customerId'
+      }, 
+      'totalDistance': {
+        '$sum': '$milesDriven'
       }
-    ];
+    }
+  }, 
+   {
+    '$addFields': {
+      'viewUpdateTimestamp': new Date()
+    }
+  },
+  {
+    '$merge': {
+      'into': 'customerTripDaily', 
+      'whenMatched': 'replace'
+    }
+  }
+];
 
     const coll = context.services.get("mongodb-atlas").db('digital_underwriting').collection('customerTripRaw');
     const cursor = coll.aggregate(agg);
