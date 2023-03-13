0e8d9fb3302b2bf63fe169587ad2225133edf826
federicoInserra <federico.inserra96@gmail.com>
2023-03-13 19:20:37 +0100

    BUGFIX solve the problem of the http request, now the function uses the builtin client from Atlas function

--- auto-deployment/insuranceDemoApp/functions/pipeline_unirest.js
+++ auto-deployment/insuranceDemoApp/functions/pipeline_unirest.js
@@ -1,26 +1,37 @@
 exports = async function(changeEvent){
 
+
+  const ENDPOINT_URL = 'https://dbc-ca6bc27a-67f7.cloud.databricks.com/serving-endpoints/luca/invocations'
+  const AUTH_TOKEN = "Basic bHVjYS5uYXBvbGlAbW9uZ29kYi5jb206THVjYXRlc3QwLg=="
+  
   const {updateDescription, fullDocument} = changeEvent;
+ 
   var collection = context.services.get("mongodb-atlas").db("digital_underwriting").collection("customerTripMonthly");
   const policy_coll = context.services.get("mongodb-atlas").db("digital_underwriting").collection("customerPolicy");
+  
   const totalDistance = parseFloat(fullDocument.totalDistance);
   const policyDoc = await policy_coll.findOne({"customerId": fullDocument._id.customerId});
+ 
+  
   const basePremium = parseFloat(policyDoc.baseMonthlyPremium);
-
-  var unirest = require('unirest');
-  var req = unirest('POST', '<MODEL-ENDPOINT>')
-  .headers({
-    'Authorization': '<CREDENTIALS>',
-    'Content-Type': 'application/json'
+  
+  const data = {"inputs": [basePremium, totalDistance]}
+  
+  const res = await context.http.post({
+    "url": ENDPOINT_URL,
+    "body": JSON.stringify(data),
+    "encodeBodyAsJSON": false,
+    "headers": {
+      "Authorization": [AUTH_TOKEN],
+      "Content-Type": [ "application/json" ]
+    }
   })
-  .send(JSON.stringify({"inputs": [basePremium, totalDistance]}))
-  .end(function (res) {
-    if (res.error) throw new Error(res.error);
 
-    let calculated_premium = JSON.parse(res.raw_body).predictions;
-    const month = fullDocument._id.month;
-    policy_coll.updateOne({"_id":policyDoc._id}, {$push:{"premium":{"month":month,"calculatedPremium": calculated_premium}}});
-
-  });
+  
+  let calculated_premium = EJSON.parse(res.body.text()).predictions;
+  
+  const month = fullDocument._id.month;
+  policy_coll.updateOne({"_id":policyDoc._id}, {$push:{"premium":{"month":month,"calculatedPremium": calculated_premium}}});
+  
 
  };
